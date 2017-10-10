2f32ddcd66e3bb29ab1dcef3964fd7dc44b2a5c7
Katherine Chen <cykatherinee@hotmail.com>
2017-10-10 22:53:45 +1100

    Add solution of QG for 16-ICPCNorthwestRegionalDiv2

---
+++ 16-ICPCNorthwestRegionalDiv2/G.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char a[55][55];
+
+int main() {
+    int n, m;  cin >> n >> m;
+    char ans[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == '#') ans[i][j] = '#';
+            else ans[i][j] = '.';
+        }
+    }
+
+    for (int j = 0; j < m; j++) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i][j] == '#') {
+                int k = 0;
+                while (k < sum) {
+                    ans[i-k-1][j] = 'o';
+                    k += 1;
+                }
+                sum = 0;
+            }
+            else if (a[i][j] == 'o') sum += 1;
+        }
+        if (sum > 0) {
+            for (int k = 0; k < sum; k++) {
+                ans[n-k-1][j] = 'o';
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << ans[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
